9314bfbfce76463a013378f820be064d84fa7e28
mcy <201102430@cs-cnu.org>
2018-05-02 19:38:14 +0900

    register swagger

--- toonserver/app.py
+++ toonserver/app.py
@@ -1,3 +1,23 @@
 from flask import Flask
+from flask_restful_swagger_2 import Api
+from flask_swagger_ui import get_swaggerui_blueprint
+
 
 app = Flask(__name__)
+
+api = Api(app, api_version='0.1', api_spec_url='/api/swagger')
+
+# setup config
+app.config['DEBUG'] = True
+app.config['TESTING'] = True
+
+# setup swagger
+swaggerui_bp = get_swaggerui_blueprint(
+    '/api/docs',
+    '/api/swagger.json',
+    config = {
+        "app_name" : "toonserver"
+    }
+)
+
+app.register_blueprint(swaggerui_bp, url_prefix='/api/docs')
